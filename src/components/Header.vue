2050fa9a6483c0369dbcd3455605b984a1a0f280
Cornille Lukas <lukas.cornille@student.howest.be>
2019-03-07 19:57:08 +0100

    add vue

---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+    <header>
+      <div id="nav-logo">
+        <img src="../assets/finished_logo.png" />
+        EduGaming
+      </div>
+    </header>
+</template>
+
+<script>
+  export default {
+    name: 'Header'
+  }
+</script>
+
+<style>
+</style>
