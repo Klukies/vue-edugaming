2259e0d75fc8e51a8bd55ebc14babdd1fb720fce
Cornille Lukas <lukascornille@hotmail.com>
2019-03-08 12:25:09 +0100

    basic read communication done

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,12 +1,32 @@
 <template>
-  <div id="banner">
-    <img />
+  <div id="home">
+    <div v-for="banner of banners" id="banners">
+      <div class="banner">
+        <img :src="banner.img_url" />
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Home',
+  data() {
+    return {
+      banners: [],
+      errors: []
+    }
+  },
+
+  created() {
+    this.axios.get(`http://localhost:8000/api/home`)
+    .then(response => {
+      this.banners = response.data
+    })
+    .catch(e => {
+      this.errors.push(e)
+    })
+  }
 }
 </script>
 
