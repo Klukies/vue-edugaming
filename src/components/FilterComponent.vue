849ae68cea6bbce085faad826b557c108c9f76e8
Cornille Lukas <lukascornille@hotmail.com>
2019-04-13 14:41:48 +0200

    created mobile filter component but todo

--- src/components/FilterComponent.vue
+++ src/components/FilterComponent.vue
@@ -1,33 +1,46 @@
 <template>
   <div class="filters">
-    <div class="filter" v-for="(filter, key) in filters" :key='key'>
-      <h1>{{ key }}</h1>
-      <ul v-if="key=='Games'">
-        <li v-for="option in filter">
-          <input @change="applyFilter" type="checkbox" :id="option.title" :value="option.game_id" v-model="gameToFilter"/>
-          <label :for="option.title">{{ option.title }}</label>
-        </li>
-      </ul>
-      <ul v-else-if="key=='Prices'" v-for="option in filter">
-        <li v-for="(key,value) in option">
-          <input @change="applyFilter" @click="unselectPrice(value)" type="radio" :id="key" :value="value" v-model="priceToFilter"/>
-          <label :for="key">{{ key }}</label>
-        </li>
-      </ul>
-      <ul v-else v-for="option in filter">
-        <li v-for="value in option">
-          <input @change="applyFilter" @click="unselectRating(value)" type="radio" :id="value" :value="value" v-model="ratingToFilter"/>
-          <label :for="value">
-            <StarRatingComponent
-            :show-rating="false"
-            :star-size="22"
-            :inactive-color="'transparent'"
-            :rating="parseInt(value)"
-            :read-only="true">
-            </StarRatingComponent>
-          </label>
-        </li>
-      </ul>
+    <div class="filter-desktop" v-if='!filterSelect'>
+      <div class="filter" v-for="(filter, key) in filters" :key='key'>
+        <h1>{{ key }}</h1>
+        <ul v-if="key=='Games'">
+          <li v-for="option in filter">
+            <input @change="applyFilter" type="checkbox" :id="option.title" :value="option.game_id" v-model="gameToFilter"/>
+            <label :for="option.title">{{ option.title }}</label>
+          </li>
+        </ul>
+        <ul v-else-if="key=='Prices'" v-for="option in filter">
+          <li v-for="(key,value) in option">
+            <input @change="applyFilter" @click="unselectPrice(value)" type="radio" :id="key" :value="value" v-model="priceToFilter"/>
+            <label :for="key">{{ key }}</label>
+          </li>
+        </ul>
+        <ul v-else v-for="option in filter">
+          <li v-for="value in option">
+            <input @change="applyFilter" @click="unselectRating(value)" type="radio" :id="value" :value="value" v-model="ratingToFilter"/>
+            <label :for="value">
+              <StarRatingComponent
+              :show-rating="false"
+              :star-size="22"
+              :inactive-color="'transparent'"
+              :rating="parseInt(value)"
+              :read-only="true">
+              </StarRatingComponent>
+            </label>
+          </li>
+        </ul>
+      </div>
+    </div>
+
+    <div class="filter-mobile" v-else>
+      <div class="filter-m" v-for="(filter, key) in filters" :key='key' >
+        <h1>{{ key }}</h1>
+        <ul v-if="key=='Games'">
+          <li v-for="option in filter">
+
+          </li>
+        </ul>
+      </div>
     </div>
   </div>
 </template>
@@ -45,7 +58,19 @@ export default {
       filters: [],
       gameToFilter: [],
       priceToFilter: '',
-      ratingToFilter: -1
+      ratingToFilter: -1,
+      windowWidth: 0,
+      filterSelect: false,
+    }
+  },
+
+  watch: {
+    windowWidth(newWidth) {
+      if (newWidth <= 1280 && this.filterSelect === false) {
+        this.filterSelect = true;
+      } else if (newWidth > 1280 && this.filterSelect === true) {
+        this.filterSelect = false;
+      }
     }
   },
 
@@ -57,7 +82,19 @@ export default {
     .catch((err) => {
       console.log(err);
     });
+    if (window.innerWidth <= 1280) {
+      this.filterSelect = true;
+    }
   },
+
+  mounted() {
+    this.$nextTick(() => {
+      window.addEventListener('resize', () => {
+        this.windowWidth = window.innerWidth;
+      });
+    })
+  },
+
   methods: {
     unselectPrice(price) {
       if (price == this.priceToFilter) {
